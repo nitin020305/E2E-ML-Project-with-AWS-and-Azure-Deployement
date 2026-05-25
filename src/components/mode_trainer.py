720db751b3a160bbb9f76b93439862f221f8754d
nitin020305 <nitinchoudhary653265@gmail.com>
2026-05-26 00:33:16 +0530

    Model trainer file updated

---
+++ src/components/mode_trainer.py
@@ -0,0 +1,24 @@
+import os
+import sys 
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import Customexception
+from src.logger import logging
+from src.utils import save_path
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
+
