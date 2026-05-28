9475dcf3dd780feb7fcbe935c454333294a5c152
nitin020305 <nitinchoudhary653265@gmail.com>
2026-05-28 19:25:54 +0530

    Hyper parameter tuning done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,13 +9,16 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import  ModelTrainer
+
+
 
 @dataclass
 class DataIngestionConfig:
     train_data_path : str =os.path.join('artifacts',"train.csv")
-    test_data_path  :  str  =os.path.join('artifacts',"test.csv")
-    raw_data_path   :   str   =os.path.join('artifacts',"data.csv")
+    test_data_path  : str =os.path.join('artifacts',"test.csv")
+    raw_data_path   : str =os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,7 +16,8 @@ from xgboost import XGBRegressor
 
 from src.exception import Customexception
 from src.logger import logging
-from src.utils import save_path, evalute_models
+from src.utils import save_path
+from src.utils import evaluate_models
 
 
 @dataclass
@@ -46,9 +47,50 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor()
             }
 
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2']
+                },
 
-            model_report: dict= evalute_models(x_train=x_train,y_train= y_train,x_test=x_test,y_test=y_test,
-                                              models=models)
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+
+                "Linear Regression":{},
+
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance'],
+                    'algorithm':['auto','ball_tree','kd_tree','brute']
+                }
+            }
+
+            model_report: dict= evaluate_models(x_train=x_train,y_train= y_train,x_test=x_test,y_test=y_test,
+                                              models=models, param=params)
             
             ##to get best model score form dictionary
             best_model_score = max(sorted(model_report.values()))
